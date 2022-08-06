93c8c52204f300029faf7900d0e33c32ee1e3e8c
RichardLeonardoSavage <savagerichard100@gmail.com>
2022-08-06 18:43:06 +1200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿namespace LearningDataTypes
+{
+    class Program
+    {
+       
+
+        static void Main(string[] args)
+        {
+            //string refers to phrases
+            string variableOne = "This is a string phrase and here is some random info - ";
+            string variableTwo = "John is ";
+            string variableThree = " and he has $";
+
+            //bool is a boolean and can be true or false;
+            bool isMale = true;
+
+
+            //int refers to an integer (whole number)
+            int wholeNumberVariable = 69;
+
+            //float - double - decimal all can have decimals with decimal being the most accurate, and float being the least. 
+            double moneyVariable = 42.56;
+
+            Console.WriteLine(variableOne + variableTwo + wholeNumberVariable + variableThree + moneyVariable);
+        }
+    }
+}
+
